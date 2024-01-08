d6a221f148030bea09ac4d5a08dbf5277e79be07
Joachim Rorbol <jr@efio.dk>
2024-01-08 16:15:43 +0100

    Fixed index issue

--- efs.tf
+++ efs.tf
@@ -8,10 +8,10 @@ resource "aws_efs_file_system" "this" {
   })
 }
 
-resource "aws_efs_mount_target" "this" {
-  count = local.config.efs_enabled ? length(local.config.private_subnet_ids) : 0
+# resource "aws_efs_mount_target" "this" {
+#   count = local.config.efs_enabled ? length(local.config.private_subnet_ids) : 0
 
-  security_groups = [aws_security_group.efs[0].id]
-  file_system_id  = aws_efs_file_system.this[0].id
-  subnet_id       = local.config.private_subnet_ids[count.index]
-}
+#   security_groups = [aws_security_group.efs[0].id]
+#   file_system_id  = aws_efs_file_system.this[0].id
+#   subnet_id       = local.config.private_subnet_ids[count.index]
+# }
