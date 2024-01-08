9870a2c90c7b486edb219029052feb4d2f1f0fac
Joachim Rorbol <jr@efio.dk>
2024-01-08 16:01:08 +0100

    Updated to tf v1.6

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "config" {
   type = object({
     vpc_id                 = string
-    private_subnet_ids     = list(string)
-    public_subnet_ids      = list(string)
-    cluster_version        = optional(string)
+    private_subnet_ids     = set(string)
+    public_subnet_ids      = set(string)
+    cluster_version        = optional(string, "1.29")
     worker_node_count      = number
     worker_instance_type   = string
     worker_volume_size     = number
-    api_allowed_ips        = optional(list(string))
-    addons                 = optional(list(string))
-    efs_enabled            = optional(bool)
+    api_allowed_ips        = optional(set(string), ["0.0.0.0/0"])
+    addons                 = optional(set(string))
+    efs_enabled            = optional(bool, false)
     administrator_role_arn = optional(string)
   })
 }
